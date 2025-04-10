3be7515ff985b449af99035d50940eaafa0868c2
Vipul Singh <104285752+Vipul123kumar@users.noreply.github.com>
2025-04-10 17:43:31 +0530

    Add files via upload

---
+++ context.jsx
@@ -0,0 +1,22 @@
+// import { createContext } from "react";
+// import run from "../Config/gamini";
+
+//  export const context=createContext();
+//  const ContexProvider=(props)=>
+//  {
+//     const onSent=async (prompt)=>
+//     {
+//       await  run(prompt)
+//     }
+//     onSent("what is react js")
+    
+//     const contextValue={
+        
+//     }
+//     return(
+//         <context.Provider value={contextValue}>
+//             {props.children}
+//         </context.Provider>
+//     )
+//  }
+//  export default ContexProvider
